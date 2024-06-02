72e435e4fb433e6d5cb8e4eb89ce86e584ea888a
GDiamend <GDiamend@gmail.com>
2024-06-02 19:35:49 -0300

    Se agrega manejo de DOM y se finaliza la pre entrega

--- js/index.js
+++ js/index.js
@@ -1,3 +1,72 @@
+//Caputo el id "headerIndex" y le agrego la clase "headerIndex"
+const headerIndex = document.getElementById('headerIndex');
+headerIndex.classList.add('headerIndex');
+
+//Capturo el id "mainIndex" y le agrego la clase mainIndex
+const mainIndex = document.getElementById('mainIndex');
+mainIndex.classList.add('mainIndex');
+
+//Creo un div y un h1
+const divH1Index = document.createElement('div');
+const h1Index = document.createElement('h1');
+
+//Le agrego contenido al h1
+h1Index.innerText = "Planifica tu Dieta con Nuestras Guías de Nutrición";
+
+//Anido
+mainIndex.appendChild(divH1Index);
+divH1Index.appendChild(h1Index);
+
+//Capturo el id "footerIndex" y le agrego la clase "footerIndex"
+const footerIndex = document.getElementById('footerIndex');
+footerIndex.classList.add('footerIndex');
+
+//Creo un div y le agrego una clase de bootstrap
+const divFooter = document.createElement('div');
+divFooter.classList.add('text-end');
+
+//Creo una section y le agrego un p
+const sectionFooter = document.createElement('section');
+const pFooter = document.createElement('p');
+
+//Le agrego contenido al p y logo de copyright
+pFooter.innerHTML = "Todos los derechos reservados <i class='fa-regular fa-copyright'></i>";
+
+//Anido
+footerIndex.appendChild(divFooter);
+divFooter.appendChild(sectionFooter);
+sectionFooter.appendChild(pFooter);
+
+//Creo un div en el que se veran los datos que va ingresando el usuario vía prompt
+const divDatosUsuario = document.createElement('div');
+
+//Creo un h2 a modo de título de la sección
+const h2DatosUsuario = document.createElement('h2');
+h2DatosUsuario.innerText = "Datos del usuario";
+
+//Creo los campos para mostrar los valores próximos a ingresar
+const pNombre = document.createElement('p');
+const pGenero = document.createElement('p');
+const pPeso = document.createElement('p');
+const pAltura = document.createElement('p');
+const pImc = document.createElement('p');
+
+pNombre.innerText = "Nombre: ";
+pGenero.innerText = "Genero: ";
+pPeso.innerText = "Peso: ";
+pAltura.innerText = "Altura: ";
+pImc.innerText = "IMC: ";
+
+//Anido
+mainIndex.appendChild(divDatosUsuario);
+divDatosUsuario.appendChild(h2DatosUsuario);
+divDatosUsuario.appendChild(pNombre);
+divDatosUsuario.appendChild(pGenero);
+divDatosUsuario.appendChild(pPeso);
+divDatosUsuario.appendChild(pAltura);
+divDatosUsuario.appendChild(pImc);
+
+//Creo la clase Persona
 class Persona {
     constructor(nombre, genero, peso, altura, imc) {
         this.nombre = nombre;
@@ -6,9 +75,21 @@ class Persona {
         this.altura = altura;
         this.imc = imc;
     };
-}
 
-//funciones de orden superior
+    mostrarResultado() {
+        if (this.imc < 18.5) {
+            console.log(this.nombre + ", usted está por debajo de su peso normal");
+        } else if (this.imc >= 18.5 && this.imc < 24.9) {
+            console.log(this.nombre + ", usted está en su peso normal");
+        } else if (this.imc >= 25 && this.imc < 29.9) {
+            console.log(this.nombre + ", usted está en sobrepeso");
+        } else {
+            console.log(this.nombre + ", usted padece obesidad");
+        }
+    };
+};
+
+//Funciones de orden superior
 
 function mayorQue(numero) {
     return (numeroAComparar) => numeroAComparar > numero;
@@ -52,19 +133,69 @@ const calcularImc = (peso, altura) => {
     return (peso / (altura * altura));
 };
 
-const darResultado = (imc) => {
-    if (imc < 18.5) {
-        alert(nombre + ", usted está por debajo de su peso normal");
-    } else if (imc >= 18.5 && imc < 24.9) {
-        alert(nombre + ", usted está en su peso normal");
-    } else if (imc >= 25 && imc < 29.9) {
-        alert(nombre + ", usted está en sobrepeso");
-    } else {
-        alert(nombre + ", usted padece obesidad");
+//Creo una Persona tomando los datos ingresados vía prompt
+const usuario = new Persona(obtenerNombre(), obtenerGenero(), obtenerPeso(), obtenerAltura(), calcularImc(peso, altura));
+
+//Actualizo los datos en el html
+pNombre.innerText += usuario.nombre;
+pGenero.innerText += usuario.genero;
+pPeso.innerText += usuario.peso;
+pAltura.innerText += usuario.altura;
+pImc.innerText += usuario.imc;
+
+//Creo la clase Libro
+class Libro {
+    constructor(nombre, texto) {
+        this.nombre = nombre;
+        this.texto = texto;
+    }
+}
+
+//Creo un texto y varias instancias de Libro
+let texto = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quibusdam culpa tenetur totam, omnis delectus rem, molestiae, magni dignissimos non impedit! Magni tempora sequi a soluta magnam nemo repellat libero. Reiciendis consequatur ipsum eius rem sed, rerum cupiditate error magni in quaerat soluta, qui cum aut nesciunt eligendi! Eligendi hic inventore debitis quos expedita reiciendis architecto quibusdam sint non aperiam!Excepturi, rem ? Atque quia excepturi et itaque maiores libero illo ullam architecto error sapiente mollitia magnam iure tempora neque fugiat aspernatur, porro recusandae reiciendis nihil assumenda provident delectus autem obcaecati.";
+
+const comidaSinSal = new Libro("Comida sin sal", texto);
+const comidaBajaCalorias = new Libro("Comida baja en calorías", texto);
+const comidaSaludable = new Libro("Comida saludable", texto);
+const ejercicioCardio = new Libro("Guia de ejercicios cardio", texto);
+const planDeAlimentacion = new Libro("Plan de alimentación", texto);
+const comoInfluyeComidaAnimo = new Libro("Como influye la comida en nuestro ánimo", texto);
+
+//Array de libros
+const libros = [comidaSinSal, comidaBajaCalorias, comidaSaludable, ejercicioCardio, planDeAlimentacion, comoInfluyeComidaAnimo];
+
+//Funcion de orden superior
+function mostrarTitulos(array, funcion) {
+    let i = 1;
+    for (const libro of array) {
+        funcion(i + " - " + libro.nombre);
+        i++;
     }
 };
 
+//Toda esta parte la quería hacer en el html pero los prompt bloquean que se cargue el html primero
+//por lo que el usuario no podría ver las opciones. En un futuro cuando no se usen mas prompt lo soluciono
+function corroborarRta() {
+    let rta;
+    do {
+        rta = prompt("Escriba el número deseado o 'ESC' para salir");
+    } while (rta.toUpperCase() != "ESC" && rta < 1 || rta.toUpperCase() != "ESC" && rta > libros.length);
 
-const usuario = new Persona(obtenerNombre(), obtenerGenero(), obtenerPeso(), obtenerAltura(), calcularImc(peso, altura));
-console.log(usuario);
-darResultado(usuario.imc);
+    if (rta.toUpperCase() != "ESC") {
+        rta--;
+        console.log("Introducción del libro " + libros[rta].nombre + ": " + libros[rta].texto);
+        console.log("Actualmente no contamos con stock de este libro");
+        console.log("Vuelva a visitarnos pronto para mas información");
+    }
+    console.log("Muchas gracias por su visita");
+}
+
+function mostrarLog() {
+    console.log("Tenemos un catalogo de libros a la venta que le pueden ayudar a mejorar su calidad de vida");
+    console.log("Si está interesado en leer la introducción de alguno de los siguientes libros escriba el nombre del mismo, de lo contrario escriba ESC para terminar");
+}
+
+usuario.mostrarResultado();
+mostrarLog();
+mostrarTitulos(libros, console.log);
+corroborarRta();
